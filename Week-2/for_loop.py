05862b707d1e24b71547c259c5f74e784320a074
peter keredah <peterkeredah85@gmail.com>
2023-02-19 15:32:11 +0300

    lesson

--- Week-2/for_loop.py
+++ Week-2/for_loop.py
@@ -15,3 +15,105 @@ product = 1
 for x in range (1,10):
     product = product * x
     print (product)
+# Iterating over dictionary
+print("Dictionary Iteration")
+d = dict()
+d['xyz'] = 123
+d['abc'] = 345
+for i in d:
+    print("% s % d" % (i, d[i]))
+    
+# Prints all letters except 'e' and 's'
+for letter in 'geeksforgeeks':
+    if letter == 'e' or letter == 's':
+        continue
+    print('Current Letter :', letter)
+    
+for letter in 'geeksforgeeks':
+ 
+    # break the loop as soon it sees 'e'
+    # or 's'
+    if letter == 'e' or letter == 's':
+        break
+ 
+print('Current Letter :', letter)
+
+# Python Program to
+# show range() basics
+ 
+# printing a number
+for i in range(10):
+    print(i, end=" ")
+ 
+# performing sum of first 10 numbers
+sum = 0
+for i in range(1, 10):
+    sum = sum + i
+print("\nSum of first 10 numbers :", sum)
+
+# Python program to demonstrate
+# for-else loop
+ 
+for i in range(1, 4):
+    print(i)
+else:  # Executed because no break in for
+    print("No Break\n")
+# Python program to illustrate
+# Iterating over a list
+print("List Iteration")
+l = ["geeks", "for", "geeks"]
+for i in l:
+    print(i)
+ 
+# Iterating over a tuple (immutable)
+print("\nTuple Iteration")
+t = ("geeks", "for", "geeks")
+for i in t:
+    print(i)
+ 
+# Iterating over a String
+print("\nString Iteration")
+s = "Geeks"
+for i in s:
+    print(i)
+ 
+# Iterating over dictionary
+print("\nDictionary Iteration")
+d = dict()
+d['xyz'] = 123
+d['abc'] = 345
+for i in d:
+    print("%s  %d" % (i, d[i]))
+ 
+# Iterating over a set
+print("\nSet Iteration")
+set1 = {1, 2, 3, 4, 5, 6}
+for i in set1:
+    print(i),
+
+
+
+fruits = ["apple", "orange", "kiwi"]
+ 
+# Creating an iterator object
+# from that iterable i.e fruits
+iter_obj = iter(fruits)
+ 
+
+fruits = ["apple", "orange", "kiwi"]
+ 
+# Creating an iterator object
+# from that iterable i.e fruits
+iter_obj = iter(fruits)
+ 
+# Infinite while loop
+while True:
+    try:
+        # getting the next item
+        fruit = next(iter_obj)
+        print(fruit)
+    except StopIteration:
+ 
+        # if StopIteration is raised,
+        # break from loop
+        break
--- Week-2/typecasting.py
+++ Week-2/typecasting.py
@@ -1,3 +1,28 @@
+
+# Python program to demonstrate
+# implicit type Casting
+ 
+# Python automatically converts
+# a to int
+a = 7
+print(type(a))# Python automatically converts
+# b to float
+b = 3.0
+print(type(b))
+ 
+# Python automatically converts
+# c to float as it is a float addition
+c = a + b
+print(c)
+print(type(c))
+ 
+# Python automatically converts
+# d to float as it is a float multiplication
+d = a * b
+print(d)
+print(type(d))
+
+
 height = 34.78
 #converting into an integers
 h =int(height)
