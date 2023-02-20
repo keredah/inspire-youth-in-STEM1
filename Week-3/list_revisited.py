9e5bbaa53021de6d178aa1f3583cf942688dd403
peter keredah <peterkeredah85@gmail.com>
2023-02-20 09:58:33 +0300

    lesson

---
+++ Week-3/list_revisited.py
@@ -0,0 +1,18 @@
+#/user/bin/env/python3
+myfevouriteFruits=["banana","apple","melon","orange","pinaple"]
+for fruit in myfevouriteFruits:
+    print (fruit)
+my_fevourite_fruits=["banana","apple","melon"]
+for fruit in my_fevourite_fruits:
+    print(fruit)
+#to acess elements you use a for loo.
+print(len("myfevouriteFruites"))
+friends=["jane","joy","paul","joy",]
+friends[0]= "mary"
+print(friends)
+new_friends=friends.copy()
+print(new_friends)
+new_friends.sort()
+print(new_friends)
+new_friends.pop()
+print(new_friends)
